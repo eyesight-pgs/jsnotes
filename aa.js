13ace2c4d7dddada3ba856095823c1a376180059
eyesight-pgs <eyesight.pgs@gmail.com>
2024-11-24 00:12:52 +0530

    initial commit

---
+++ aa.js
@@ -0,0 +1,51 @@
+
+
+let notes = [
+  {
+    id: "MY-001 notes as js repo",
+    done: false,
+    content: `
+
+`
+
+  }
+
+];
+
+
+/*
+
+
+jsnotes alias "MY-001" "cat"
+or jsnotes cat "MY-001"
+jsnotes last "MY-001"
+
+
+jsnotes imp list
+jsnotes imp add "MY-001"
+jsnotes imp drop "MY-001"
+
+
+jsnotes show cat
+jsnotes list
+jsnotes list open
+jsnotes list today
+jsnotes list todaydone
+jsnotes add "MY-002" "Create new app to store family tree" "" "The app should have person (the most basic item) 2 person objects can be linked with relation"
+jsnotes mark done "MY-002"
+jsnotes mark inprogress "MY-002"
+
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
